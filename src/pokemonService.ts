62eb56193a9756a9b952ea6155c8cd0567df3251
BastienVienet <bastien.vienet@outlook.com>
2023-01-06 12:24:08 +0100

    Doing all of my api call here

---
+++ src/pokemonService.ts
@@ -0,0 +1,24 @@
+// Fetch datas
+import axios from "axios";
+import {ApiPokemonDataResults, ApiPokemonTypeResults, ApiResults, RestRef} from "./types";
+
+export const pokemonService = {
+
+    fetchPokemons: async () => {
+        const pokemonListQuery = await axios.get<ApiResults>('https://pokeapi.co/api/v2/pokemon?limit=9')
+        return pokemonListQuery.data.results
+    },
+
+    fetchPokemonDetails: async (pokemonRef: RestRef) => {
+        const pokemonDataQuery = await axios.get<ApiPokemonDataResults>(pokemonRef.url)
+        return pokemonDataQuery.data
+    },
+
+    fetchPokemonTypes: async (typeRefs: RestRef[]) => {
+        const promises = await Promise.all(typeRefs.map((typeRef) => axios.get<ApiPokemonTypeResults>(typeRef.url)))
+        return promises.map((p) => p.data)
+    },
+
+}
+
+
