fb41cdd655899bd753674d50cb54d334035fcf9c
BastienVienet <bastien.vienet@outlook.com>
2023-01-20 13:44:27 +0100

    Dark Mode toggle button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,11 +5,9 @@ import Button from "@mui/material/Button";
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import SearchIcon from '@mui/icons-material/Search';
-import {IconButton, TextField} from "@mui/material";
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import {FormControlLabel, FormGroup, TextField} from "@mui/material";
 import {ThemeContext} from "../context/ThemeContext";
-import {Theme} from "../types";
+import {MaterialUISwitch} from "../style/MaterialUISwitch";
 
 type Props = {
     setFilter: (filter: string) => void, setUser: (user: object) => void,
@@ -31,7 +29,7 @@ export const NavBar = ({setFilter, setUser}: Props) => {
             <AppBar
                 position="fixed"
                 sx={{px: 2, minHeight: '10vh'}}>
-                <Toolbar sx={{display: 'flex', justifyContent: 'space-between', flexGrow: 1, pb: 2}}>
+                <Toolbar sx={{display: 'flex', justifyContent: "space-between", flexGrow: 1, pb: 2}}>
                     <Box sx={{display: 'flex', alignItems: 'flex-end'}}>
                         <SearchIcon sx={{color: 'action.active', mr: 1, my: 0.5}}/>
                         <TextField
@@ -39,18 +37,23 @@ export const NavBar = ({setFilter, setUser}: Props) => {
                             label="Pokemon name"
                             variant="standard"/>
                     </Box>
-                    <Box sx={{mt: 2}}>
-                        {theme} mode
-                        <IconButton sx={{ml: 1}} onClick={toggleTheme} color="inherit">
-                            {theme === Theme.Dark ? <Brightness7Icon/> : <Brightness4Icon/>}
-                        </IconButton>
-                    </Box>
-                    <Button sx={{mt: 2}}
-                        onClick={() => handleSignOut()}
-                        color="inherit"
-                    >
-                        Sign Out
-                    </Button>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <Box>
+                            <FormGroup sx={{mt: 3}}>
+                                <FormControlLabel
+                                    control={<MaterialUISwitch/>}
+                                    onClick={toggleTheme}
+                                    label={`${theme} mode`}
+                                />
+                            </FormGroup>
+                        </Box>
+                        <Button sx={{mt: 3}}
+                                onClick={() => handleSignOut()}
+                                color="inherit"
+                        >
+                            Sign Out
+                        </Button>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>
