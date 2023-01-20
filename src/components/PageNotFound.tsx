6d1c2395e3caafd5aea4f81891220041de4cb2b7
BastienVienet <bastien.vienet@outlook.com>
2023-01-20 16:40:19 +0100

    Changing 404 text

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 export const PageNotFound = () => {
     return (
-        <Typography variant="h2">
-            Page not found... Error 404 !
+        <Typography>
+            <h1>Oops..! 404 Page Not Found</h1>
+            <p>Looks like you're lost ..</p>
         </Typography>
     )
 }
