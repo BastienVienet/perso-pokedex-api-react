298124f810fe6bfe19a3dd18ddc2d47da365fbb3
BastienVienet <bastien.vienet@outlook.com>
2023-01-26 13:12:58 +0100

    Solving error in <p>

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const PageNotFound = () => {
     return (
-        <Typography>
+        <Typography component="div">
             <h1>Oops..! 404 Page Not Found</h1>
             <p>Looks like you're lost ..</p>
         </Typography>
